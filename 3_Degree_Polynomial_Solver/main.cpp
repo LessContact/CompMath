faacfc261171e8a5abb2d3b9db54fbb2188a6cca
LessContact <korol@LESSCONTACT>
2024-09-05 00:02:47 +0700

    WIP

--- 3_Degree_Polynomial_Solver/main.cpp
+++ 3_Degree_Polynomial_Solver/main.cpp
@@ -37,6 +37,10 @@ inline void OutputRoot(root root){
     std::cout << "Root value: " << root.value << std::endl << "Multiplicity: " << root.multiplicity << std::endl;
 }
 
+root SolveClosedInterval(double pointLeft, double pointRight){
+    //todo: implement
+}
+
 //direction is the infinity direction
 root SolveOpenInterval(double point, int direction){
     //todo: implement
@@ -96,6 +100,7 @@ int main() {
     root difRoot1, difRoot2;
     difRoot1.value = (-diffedCoeffs.B + std::sqrt(discriminant)) / (diffedCoeffs.A * 2);
     difRoot2.value = (-diffedCoeffs.B - std::sqrt(discriminant)) / (diffedCoeffs.A * 2);
+    if(difRoot1.value > difRoot2.value) std::swap(difRoot1, difRoot2); // sort roots)))
 
     // CASE IV
     if(EqualsZero(discriminant, epsilon)){ //todo: god knows if this will work
@@ -141,10 +146,48 @@ int main() {
 
     //CASE II
 
-    //todo :CASE II and CASE III
+    //II.3
+    if(y(difRoot1.value, coeffs) > epsilon && y(difRoot2.value,coeffs) < -epsilon){
+        root root1 = SolveOpenInterval(difRoot1.value, left);
+        root root2 = SolveClosedInterval(difRoot1.value, difRoot2.value);
+        root root3 = SolveOpenInterval(difRoot2.value, right);
+        OutputRoot(root1);
+        OutputRoot(root2);
+        OutputRoot(root3);
+        return 0;
+    }
+    //II.1
+    else if(y(difRoot2.value, coeffs) > 0){
+        root root = SolveOpenInterval(difRoot1.value, left);
+        OutputRoot(root);
+        return 0;
+    }//II.2
+    else if(y(difRoot1.value, coeffs) < 0){
+        root root = SolveOpenInterval(difRoot2.value, right);
+        OutputRoot(root);
+        return 0;
+    }
 
+    //CASE III
 
+    //III.1
+    if(EqualsZero(y(difRoot2.value, coeffs), epsilon) && y(difRoot1.value, coeffs) > epsilon){
+        difRoot2.multiplicity = 2;
+        root root = SolveOpenInterval(difRoot2.value, right);
+        OutputRoot(root);
+        OutputRoot(difRoot2);
+        return 0;
+    }
+    //III.2
+    else if(EqualsZero(y(difRoot1.value, coeffs), epsilon) && y(difRoot2.value, coeffs) < -epsilon){
+        difRoot1.multiplicity = 2;
+        root root = SolveOpenInterval(difRoot2.value, right);
+        OutputRoot(difRoot1);
+        OutputRoot(root);
+        return 0;
+    }
 
+    std::wcout << L"You've reached the end of the line.\nThis should never happen.\nBetter fix this quick\n┻━┻ ︵ ＼( °□° )／ ︵ ┻━┻" << std::endl;
 
     return 0;
 }
